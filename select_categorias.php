8caf8a8fb387fecfa9f29506fe4447fae5d53101
Miguel Vidal <68684733+migvidal@users.noreply.github.com>
2021-06-08 18:24:58 +0200

    primer-commit-2

---
+++ select_categorias.php
@@ -0,0 +1,21 @@
+<?php require_once 'funciones.php'?>
+
+
+    <?php
+    /* seleccionar categorias */
+    $campos = ['id_categ', 'nombre_categ'];
+    $resultado = selectModelo($campos, 'categoria', null, null);
+    ?>
+
+    <!--html-->
+<select name="categoria" id="categoria">
+    <?php
+    foreach ($resultado as $fila) {
+        extract($fila);
+    ?>
+        <option value="<?php echo $id_categ;?>"><?php echo $nombre_categ; ?></option>
+
+    <?php
+    }
+    ?>
+</select>
